5c0d3d9bd63c375c4016e34b8258bc92ac91a446
MohammadJavad14 <mrph14@gmail.com>
2022-06-16 15:10:36 +0430

    custom type declarations

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
-	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
